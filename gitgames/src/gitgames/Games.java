bff041ac7c4fe18ed4e8f3e0cfcac995e8cd796d
dimitrit_vu <dimitrit@amdocs.com>
2019-03-15 12:55:21 +0200

    Change the code in IntelliJ

--- gitgames/src/gitgames/Games.java
+++ gitgames/src/gitgames/Games.java
@@ -4,7 +4,7 @@ public class Games {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello there");
+		System.out.println("Hello there, I've changed");
 
 	}
 
